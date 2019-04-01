07fa618249c048c7ebb4fba71bf86bcc51051ce4
yinghui tan <yinghuitan18@gmail.com>
2019-04-01 14:05:56 +0800

    ask and answer working

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+    @ask = params[:ask]
+  end
+
+  def answer
+    if params[:ask] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:ask].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #First page is a form with an input, where a user can type a question to ask the Coach
+
+  get 'ask', to: 'questions#ask'
+
+  #After submitting the form, the user is redirected to a new page where he will see his question and the coach answer.
+  get 'answer', to: 'questions#answer'
+
+
 end
